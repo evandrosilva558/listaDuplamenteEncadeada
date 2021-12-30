2067bb0205fe1b1fcaa7802d70dae2f97f345240
Evandro <evandrosilva558@hotmail.com>
2021-12-30 19:23:54 -0300

    criado os metodos getno, get, add, remove e toString

--- src/com/listaDuplamenteEncadeada/ListaDuplamenteEncadeada.java
+++ src/com/listaDuplamenteEncadeada/ListaDuplamenteEncadeada.java
@@ -2,6 +2,7 @@ package com.listaDuplamenteEncadeada;
 
 public class ListaDuplamenteEncadeada<T> {
 	
+
 	private NoDuplo<T> primeiroNo;
 	private NoDuplo<T> ultimoNo;
 	
@@ -13,8 +14,94 @@ public class ListaDuplamenteEncadeada<T> {
 		this.tamanhoLista = 0;
 	}
 	
-	public int size() {
-		return tamanhoLista;
+	public T get(int index) {
+		return this.getNo(index).getConteudo();
 	}
+	
+	public void add(T elemento) {
+		NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+		novoNo.setNoProximo(null);
+		novoNo.setNoPrevio(ultimoNo);
+		
+		if (primeiroNo == null) {
+			primeiroNo = ultimoNo;
+		}
+		if (ultimoNo != null) {
+			ultimoNo.setNoProximo(novoNo);
+		}
+		ultimoNo = novoNo;
+		this.tamanhoLista++;
+	}
+	
+	public void add(int index, T elemento) {
+		NoDuplo<T> noAlxiliar = getNo(index);
+		NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+		novoNo.setNoProximo(noAlxiliar);
+		
+		if (novoNo.getNoProximo() != null) {
+			novoNo.setNoPrevio(noAlxiliar.getNoPrevio());
+			novoNo.getNoProximo().setNoPrevio(novoNo);
+			
+		} else {
+			novoNo.setNoPrevio(ultimoNo);
+			ultimoNo = novoNo;
+		}
+		
+		if (index == 0) {
+			primeiroNo = novoNo;
+			
+		} else {
+			novoNo.getNoPrevio().setNoProximo(novoNo);
+			
+		} this.tamanhoLista++;
+	}
+	
+	public void remove(int index) {
+		
+		if (index == 0) {
+			
+			primeiroNo = primeiroNo.getNoProximo();
+			if (primeiroNo != null) {
+				primeiroNo.setNoPrevio(null);
+				
+			} else {
+				NoDuplo<T> noAlxiliar = getNo(index);
+				noAlxiliar.getNoPrevio().setNoProximo(noAlxiliar.getNoProximo());
+				if (noAlxiliar != ultimoNo) {
+					noAlxiliar.getNoProximo().setNoPrevio(noAlxiliar.getNoPrevio());
+				} else {
+					ultimoNo = noAlxiliar;
+				}
+			}
+			
+		} this.tamanhoLista--;
+		
+	}
+	
+	private NoDuplo<T> getNo(int index) {
+		NoDuplo<T> noAuxiliar = primeiroNo;
+		for (int i = 0; (i < index) && (noAuxiliar != null); i++) {
+			noAuxiliar = noAuxiliar.getNoProximo();
+		} 
+		return noAuxiliar;
+	}
+	
+	public int size() {
+		return this.tamanhoLista;
+	}
+	
+	@Override
+	public String toString() {
+		String strRetorno = "";
+		NoDuplo<T> noAlxiliar = primeiroNo;
+		
+		for (int i = 0; i < size(); i++) {
+			strRetorno += "No [ conteudo =" + noAlxiliar + "] ==> ";
+			noAlxiliar = noAlxiliar.getNoProximo();
+			
+		}
+		strRetorno += "null";
+		return strRetorno;
+	} 
 	
 }
